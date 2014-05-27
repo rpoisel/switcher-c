bd750f4927a1b16ee41f8ce1fbb8a5fedfce5a33
Rainer Poisel <rainer.poisel@gmail.com>
2014-05-28 00:21:41 +0200

    piface header and source file added

---
+++ include/piface.h
@@ -0,0 +1,15 @@
+/*
+ * piface.h
+ *
+ *  Created on: May 27, 2014
+ *      Author: rpoisel
+ */
+
+#ifndef PIFACE_H_
+#define PIFACE_H_
+
+#include "i2c_io_fwd.h"
+
+io_drv* get_piface_drv();
+
+#endif /* PIFACE_H_ */
---
+++ src/piface.c
@@ -0,0 +1,16 @@
+/*
+ * piface.c
+ *
+ *  Created on: May 27, 2014
+ *      Author: rpoisel
+ */
+
+#include <stdlib.h>
+
+#include "pfio.h"
+#include "piface.h"
+
+io_drv* get_piface_drv()
+{
+	return NULL; /* not implemented yet */
+}
