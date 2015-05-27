6dfb0237a9089bea84646799fbb18263d34b7aca
Rainer Poisel <rainer.poisel@logicals.com>
2015-05-28 00:03:04 +0200

    mcp23017 write implemented

--- include/mcp23017.h
+++ include/mcp23017.h
@@ -3,13 +3,6 @@
 
 #include "io_fwd.h"
 
-typedef struct mcp23017_data_
-{
-	uint8_t mode; /* read or write */
-} mcp23017_data;
-
-
 dev_drv* get_mcp23017_drv();
-void get_mcp23017_data(mcp23017_data* data);
 
 #endif /* MCP23017_H_ */
--- src/mcp23017.c
+++ src/mcp23017.c
@@ -5,6 +5,16 @@
 #include "io.h"
 #include "mcp23017.h"
 
+typedef enum _mcp23017_io_state
+{
+	MCP23017_NONE, MCP23017_READ, MCP23017_WRITE
+} mcp23017_io_state;
+
+typedef struct mcp23017_data_
+{
+	mcp23017_io_state io_state; /* read or write */
+} mcp23017_data;
+
 static uint32_t mcp23017_init(io_bus* bus, io_dev* dev);
 static int mcp23017_write(io_bus* bus, io_dev* dev, const uint32_t* value,
 		int (*cb_error)(char* error_msg, char* buf, int buf_size),
@@ -15,32 +25,67 @@ static int mcp23017_read(io_bus* bus, io_dev* dev, uint32_t* value,
 static uint32_t mcp23017_deinit(io_bus* bus, io_dev* dev);
 
 static dev_drv handle =
-{ .init = mcp23017_init, .read = &mcp23017_read, .write = &mcp23017_write, .deinit = mcp23017_deinit };
+{ .init = mcp23017_init, .read = &mcp23017_read, .write = &mcp23017_write,
+		.deinit = mcp23017_deinit };
 
 static uint32_t mcp23017_init(io_bus* bus, io_dev* dev)
 {
+	mcp23017_data* dev_data = (dev->dev_data = malloc(sizeof(mcp23017_data)));
+	if (NULL == dev->dev_data)
+	{
+		return 1;
+	}
+	dev_data->io_state = MCP23017_NONE;
 	return 0;
 }
 
 /* write IOs */
-static int mcp23017_write(io_bus *bus, io_dev* dev,
-		const uint32_t* value,
+static int mcp23017_write(io_bus *bus, io_dev* dev, const uint32_t* value,
 		int (*cb_error)(char* error_msg, char* buf, int buf_size),
 		char* buf_msg, int buf_size_msg)
 {
 	/* check whether device has been opened in write mode and configure accordingly if necessary */
-#if 0
-	uint8_t states = *value;
-	return i2c_write(fh, address, &states, sizeof(states), cb_error, buf_msg,
-			buf_size_msg);
-#else
+	uint16_t states = *value;
+	uint8_t i2c_cmds[2] =
+	{ 0 };
+	ssize_t data_written = 0;
+	mcp23017_data* dev_data = dev->dev_data;
+	if (dev_data->io_state != MCP23017_WRITE)
+	{
+		i2c_cmds[0] = 0x00; /* IODIRA */
+		i2c_cmds[1] = 0x00; /* all outputs */
+		data_written = bus->drv_handle->write(bus, dev, i2c_cmds,
+				sizeof(i2c_cmds), cb_error, buf_msg, buf_size_msg);
+		if (sizeof(i2c_cmds) == data_written)
+		{
+			i2c_cmds[0] = 0x01; /* IODIRB */
+			i2c_cmds[1] = 0x00; /* all outputs */
+			data_written = bus->drv_handle->write(bus, dev, i2c_cmds,
+					sizeof(i2c_cmds), cb_error, buf_msg, buf_size_msg);
+			if (sizeof(i2c_cmds) == data_written)
+			{
+				dev_data->io_state = MCP23017_WRITE;
+			}
+		}
+	}
+	if (MCP23017_WRITE == dev_data->io_state)
+	{
+		data_written = 0;
+		i2c_cmds[0] = 0x14; /* OLATA */
+		i2c_cmds[1] = states & 0x00FF; /* LSB */
+		data_written += bus->drv_handle->write(bus, dev, i2c_cmds, sizeof(i2c_cmds),
+				cb_error, buf_msg, buf_size_msg);
+		i2c_cmds[0] = 0x15; /* OLATB */
+		i2c_cmds[1] = (states & 0xFF00) >> 8; /* MSB */
+		data_written += bus->drv_handle->write(bus, dev, i2c_cmds, sizeof(i2c_cmds),
+				cb_error, buf_msg, buf_size_msg);
+		return data_written;
+	}
 	return 0;
-#endif
 }
 
 /* read IOs */
-static int mcp23017_read(io_bus* bus, io_dev* dev,
-		uint32_t* value,
+static int mcp23017_read(io_bus* bus, io_dev* dev, uint32_t* value,
 		int (*cb_error)(char* error_msg, char* buf, int buf_size),
 		char* buf_msg, int buf_size_msg)
 {
@@ -63,5 +108,10 @@ dev_drv* get_mcp23017_drv()
 
 static uint32_t mcp23017_deinit(io_bus* bus, io_dev* dev)
 {
+	if (NULL != dev->dev_data)
+	{
+		free(dev->dev_data);
+		dev->dev_data = NULL;
+	}
 	return 0;
 }
